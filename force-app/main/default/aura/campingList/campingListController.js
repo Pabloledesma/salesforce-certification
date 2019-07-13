123871fb60f0760d0c452de453b08534fd65fb75
Pablo Ledesma <pabloledes83@gmail.com>
2019-07-12 21:39:17 -0500

    starting connect components whit events challenge

---
+++ force-app/main/default/aura/campingList/campingListController.js
@@ -0,0 +1,34 @@
+({
+    doInit: function(component, event, helper) {
+        // Create the action
+        var action = component.get("c.getItems");
+        // Add callback behavior for when response is received
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.items", response.getReturnValue());
+            }
+            else {
+                console.log("Failed with state: " + state);
+            }
+        });
+        // Send action off to be executed
+        $A.enqueueAction(action);
+    },
+    
+    clickCreateItem : function(component, event, helper) {
+        
+        var validItem = component.find('ItemForm').reduce(function (validSoFar, inputCmp) {
+            // Displays error messages for invalid fields
+            inputCmp.showHelpMessageIfInvalid();
+            return validSoFar && inputCmp.get('v.validity').valid;
+        }, true);
+        // If we pass error checking, do some real work
+        if(validItem){
+           
+            var newItem = component.get("v.newItem"); 
+            // Create the new item
+            helper.createItem(component, newItem);
+        }
+    }
+})
